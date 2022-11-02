24a9b35e8f044d1b759e8bdee8fcdae541961563
zsydyk <zsydykova401@gmail.com>
2022-11-02 13:47:52 +0600

    commit

---
+++ src/Triangle.java
@@ -0,0 +1,12 @@
+public class Triangle {
+    double a;
+    double b;
+    double c;
+
+    void area() {
+        double s = (a + b + c);
+        double s1 = s / 2;
+        double s2 = Math.sqrt(s1 * (s1 - a) * (s1 - b) * (s1 - c));
+        System.out.println("Area of a triangle: " + s2);
+    }
+}
